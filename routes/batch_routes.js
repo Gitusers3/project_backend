39ee753a6c88290a452e030098f4633ae24167e1
zamiya <gituserszgs2023@gmail.com>
2023-08-04 09:51:47 +0530

    agust4z

--- routes/batch_routes.js
+++ routes/batch_routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
-const {AddBatch, ViewBatch,DeleteBatch, UpdateBatch, ViewOneBatch} = require('../controller/batch_controller')
+const {AddBatch, ViewBatch,DeleteBatch, UpdateBatch, ViewOneBatch,ViewBatchTechie} = require('../controller/batch_controller')
 router.post('/insert',AddBatch);
 router.get('/view',ViewBatch);
 router.delete('/delete/:id',DeleteBatch);
 router.put('/update/:id',UpdateBatch);
 router.get('/viewbatch/:id',ViewOneBatch);
+router.get('/viewbatchtechie/:id',ViewBatchTechie);
 
 module.exports=router;
