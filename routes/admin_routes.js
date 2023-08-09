1904c2bab24ef0a6d0389b681724ea0d408635a4
Gitusers3 <gituserszgs2023@gmail.com>
2023-08-09 14:09:53 +0530

    august 7 till image upload while inserting staff and viewing staff in modal

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { Login,Register} = require('../controller/admin_controller')
-
-
+const { Login,Register,View} = require('../controller/admin_controller')
+const FetchAdmin = require('../middlewear/admin')
 router.post('/login',Login)
 router.post('/register',Register)
-
-
+router.get('/view',FetchAdmin,View)
 
 module.exports=router;
